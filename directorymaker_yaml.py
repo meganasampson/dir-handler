2de69db2ad6427258e4caaa3da7378d99dd2ad14
meganasampson <megan.anne.sampson@gmail.com>
2025-03-26 16:05:55 +0000

    end of day

--- directorymaker_yaml.py
+++ directorymaker_yaml.py
@@ -10,33 +10,45 @@ parser.add_argument("--location", default = str(os.getcwd()), type = str)
 
 args = parser.parse_args()
 
-def search_children(json_data):
+def search_children(yaml_data):
 
     """
     Check if children data exists, and send it to make_dir to
     write the directories
     """
 
-    if json_data['children']:
+    """
+    if yaml_data['children']:
         level_finder.deeper_dir()
 
-        for i in json_data['children']:
-            make_dir(i['name'])
+        for i in yaml_data['children']:
             search_children(i)
 
-        level_finder.higher_dir()
+        level_finder.higher_dir()"""
+    
+    for i in yaml_data[0]:
+        if yaml_data[0][i]:
+            level_finder.deeper_dir()
 
+            for i in yaml_data['children']:
+                search_children(i)
+
+            level_finder.higher_dir()
+
+            
 def read_yaml(yaml_file):
 
     """
-    Load the json file, and start initial search for 'children' folders
+    Load the yaml file, and start initial search for 'children' folders
     """
 
     with open('examples/structureexample.yml', 'r') as f:
         yaml_data = list(yaml.load_all(f, Loader=SafeLoader))
-        print(yaml_data)
+        #print(yaml_data)
 
-    print(yaml_data[0]['folder2'])
+    search_children(yaml_data)
+
+    #print(yaml_data[0]['folder2'])
 
 
 if __name__ == '__main__':
