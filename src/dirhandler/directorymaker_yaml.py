a5a29a5da5e285ddaeaadbd1b07205900c9449a9
meganasampson <megan.anne.sampson@gmail.com>
2025-09-05 15:09:19 +0100

    partially finished example file

--- src/dirhandler/directorymaker_yaml.py
+++ src/dirhandler/directorymaker_yaml.py
@@ -5,52 +5,50 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--filename", default = 'examples/structureexample.yaml', type = str)
+parser.add_argument("--filename", default = 'example/structureexample.yaml', type = str)
 parser.add_argument("--location", default = str(os.getcwd()), type = str)
 
 args = parser.parse_args()
 
-def search_children(yaml_data):
+""""""
+def search_children(yaml_data, level_finder):
 
     """
     Check if children data exists, and send it to make_dir to
     write the directories
     """
-
-    """
-    if yaml_data['children']:
-        level_finder.deeper_dir()
-
-        for i in yaml_data['children']:
-            search_children(i)
-
-        level_finder.higher_dir()"""
     
     for i in yaml_data[0]:
         if yaml_data[0][i]:
             level_finder.deeper_dir()
 
-            for i in yaml_data['children']:
-                search_children(i)
+            #for i in yaml_data['children']:
+            for i in yaml_data[0][i]:
+                search_children(i, level_finder)
 
             level_finder.higher_dir()
 
             
-def read_yaml(yaml_file):
+def read_yaml(yaml_file, level_finder):
 
     """
     Load the yaml file, and start initial search for 'children' folders
     """
 
-    with open('examples/structureexample.yml', 'r') as f:
+    with open(yaml_file, 'r') as f:
         yaml_data = list(yaml.load_all(f, Loader=SafeLoader))
         #print(yaml_data)
+        #print(yaml_data[0]["folder2"])
+        print(yaml_data[0])
 
-    search_children(yaml_data)
-
-    #print(yaml_data[0]['folder2'])
+    #search_children(yaml_data, level_finder)
 
 
+def start():
+    level_finder = Level()
+    read_yaml(args.filename, level_finder)
+
+"""    
 if __name__ == '__main__':
     level_finder = Level()
-    read_yaml(args.filename)
+    read_yaml(args.filename)"""
