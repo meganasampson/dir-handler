8bf5906eb807411be857c8678e616e031255b8e8
meganasampson <megan.anne.sampson@gmail.com>
2025-09-08 14:56:42 +0100

    added flag for using example jsons

--- src/dirhandler/foldermaker.py
+++ src/dirhandler/foldermaker.py
@@ -1,9 +1,12 @@
 import os
 import json
 import argparse
+import pathlib
 
 """define the arguments for making folders"""
 parser = argparse.ArgumentParser()
+parser.add_argument('-x', action='store_true',
+                    help="take example file structures when present")
 parser.add_argument("--filename", default = 'examples/nestedtstruc.json', 
                     type = str,
                     help="path to json file to read structure")
@@ -28,9 +31,13 @@ def create_folder_structure(base_path, structure):
             create_folder_structure(path, sub_structure)
 
 def start():
-    json_file = args.filename
+    if args.x == True:
+        base_dir = pathlib.Path(__file__).parent.resolve() # change eventually __file__ bad...
+        json_file = os.path.join(base_dir, args.filename)
+    else:
+        json_file = args.filename
     with open(json_file, 'r') as f:
         folder_data = json.load(f)
-
+    os.chdir(args.location)
     create_folder_structure('.', folder_data)
     print("\nFolder structure created successfully!")
