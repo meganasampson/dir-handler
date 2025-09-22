dfe6a329a2ca83fd9ae452e45f4011b0fcefc2c7
meganasampson <megan.anne.sampson@gmail.com>
2025-09-22 14:56:06 +0100

    improved function comments

--- src/dirhandler/foldermaker.py
+++ src/dirhandler/foldermaker.py
@@ -20,7 +20,21 @@ args = parser.parse_args()
 example_json = {"simpleexample":"example/simplestruc.json",
                 "nestedexample":"example/nestedstrc.json"}
 
-def create_folder_structure(base_path, structure):
+def create_folder_structure(base_path: str, 
+                            structure: dict):
+    
+    """
+    Moves to the correct location and creates folders
+
+
+    Parameters
+    ----------
+
+    base_path: str
+        Path to join with the folder name to make full path of a directory to be made
+    structure: dict
+        The dictionary data that contains the desired directory structure
+    """
 
     if isinstance(structure, list):
         for folder_name in structure:
@@ -38,7 +52,24 @@ def create_folder_structure(base_path, structure):
         f = open(path, "w")
         f.close
 
-def start(x=args.x, filename=args.filename, location=args.location):
+def start(x=args.x, 
+          filename=args.filename, 
+          location=args.location):
+    
+    """
+    Read the json structure file and call directory maker function
+
+
+    Parameters
+    ----------
+
+    x: bool
+        Whether the json is a premade example (True) or a unique json (False)
+    filename: str
+        Path to json file to read (excluding path to examples folder)
+    location: str
+        Where the directory structure should be made
+    """
 
     if x == True:
         base_dir = pathlib.Path(__file__).parent.resolve() # change eventually __file__ bad...
@@ -48,6 +79,7 @@ def start(x=args.x, filename=args.filename, location=args.location):
 
     with open(json_file, 'r') as f:
         folder_data = json.load(f)
+        print(type(folder_data))
 
     os.chdir(location)
 
--- src/dirhandler/make_struct.py
+++ src/dirhandler/make_struct.py
@@ -4,28 +4,36 @@ json_file = "examples/loops.json"
 with open(json_file, 'r') as f:
     folder_data = json.load(f)
 
-print(folder_data)
-
 myloops = {"x":"3", "y":"2"}
 
-for i in folder_data:
-    print(i)
-    res = i.translate(str.maketrans(myloops))
-    print(res)
 
+def replace_keys(json_struct,
+                 placeholder_key):
+    for i in folder_data:
+        print(i)
+        res = i.translate(str.maketrans(myloops))
+        print(res)
 
-for i in folder_data:
-    for key in myloops.keys():
-        if key in i:
-            print(f"{i} contains an {key}")
-        else:
-            print(f"{i} DOES NOT contain an {key}")
+def check_for_keys(json_struct,
+                   placeholder_key):
+    for i in json_struct:
+        for key in placeholder_key.keys():
+            if key in i:
+                print(f"{i} contains an {key}")
+            else:
+                print(f"{i} DOES NOT contain an {key}")
 
-for i in folder_data:
-    for key in myloops.keys():
-        if key in i:
-            value = int(myloops[key])
-            for i in range(int(value)):
-                print(f"printing {i}")
-        else:
-            print(f"{i} DOES NOT contain an {key}")
+def print_multi(json_struct, 
+                placeholder_key):
+    for i in json_struct:
+        for key in placeholder_key.keys():
+            if str(key) in str(i):
+                value = int(placeholder_key[key])
+                for k in range(value):
+                    print(f"printing {k}")
+            else:
+                print(f"{i} DOES NOT contain an {key}")
+
+replace_keys(folder_data, myloops)
+check_for_keys(folder_data, myloops)
+print_multi(folder_data, myloops)
